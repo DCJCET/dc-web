c2873f799b228f1b40d4e2af21db90dd2873297f
nischalmudennavar <nischalmudennavar@gmail.com>
2021-07-17 15:52:58 +0530

    made the code pretty

--- js/apply-fetch.js
+++ js/apply-fetch.js
@@ -1,68 +1,47 @@
-fetch('http://localhost:1111/allapplications', {
-    method: 'GET', // *GET, POST, PUT, DELETE, etc.
-    mode: 'cors', // no-cors, *cors, same-origin
-    headers: {
-    'Content-Type': 'application/json'
-    },   // body data type must match "Content-Type" header
-}).then(res => res.json())
-.then(data=> {
-
-
-    
-   
+fetch("http://localhost:1111/allapplications", {
+  method: "GET", // *GET, POST, PUT, DELETE, etc.
+  mode: "cors", // no-cors, *cors, same-origin
+  headers: {
+    "Content-Type": "application/json",
+  }, // body data type must match "Content-Type" header
+})
+  .then((res) => res.json())
+  .then((data) => {
     console.log(data);
 
-        const container = document.getElementById('team-container')
-    data.map((member)=>{
-        // console.log(member.name);
-        
-        document.body.appendChild(container);
-        const teamMember = document.createElement("div");
-        teamMember.classList.add('team-member');
-        container.appendChild(teamMember);
+    const container = document.getElementById("team-container");
+    data.map((member) => {
+      // console.log(member.name);
 
-        var semnbio = document.createElement('div');
-        semnbio.classList.add('semnbio');
-        teamMember.appendChild(semnbio);
+      document.body.appendChild(container);
+      const teamMember = document.createElement("div");
+      teamMember.classList.add("team-member");
+      container.appendChild(teamMember);
 
-        var memdiv = document.createElement("h1");
-        memdiv.classList.add('member-name')
-        memdiv.style.color="#fff";
-        memdiv.innerHTML =member.name;                   // Insert text
-        semnbio.appendChild(memdiv); 
+      var semnbio = document.createElement("div");
+      semnbio.classList.add("semnbio");
+      teamMember.appendChild(semnbio);
 
-       
+      var memdiv = document.createElement("h1");
+      memdiv.classList.add("member-name");
+      memdiv.style.color = "#fff";
+      memdiv.innerHTML = member.name; // Insert text
+      semnbio.appendChild(memdiv);
 
-        const sem = document.createElement("h2");
-        sem.classList.add('semester');
-        sem.innerHTML = member.semester;
-        semnbio.appendChild(sem);
+      const sem = document.createElement("h2");
+      sem.classList.add("semester");
+      sem.innerHTML = member.semester;
+      semnbio.appendChild(sem);
 
-        const bio = document.createElement("p");
-        bio.classList.add('bio');
-        bio.innerHTML = "'"+ member.bio + "'";;
-        teamMember.appendChild(bio);
+      const bio = document.createElement("p");
+      bio.classList.add("bio");
+      bio.innerHTML = "'" + member.bio + "'";
+      teamMember.appendChild(bio);
 
-        const ghbtn = document.createElement("button");
-        ghbtn.classList.add('primary-btn');
-        ghbtn.innerHTML = `<a class="gh-link" href="${member.github}"><i  class="fab fa-github"></i></a>`;
-        teamMember.appendChild(ghbtn);
-
-
-
-
-    
-        
-
-
-
-
-
-
-
-
+      const ghbtn = document.createElement("button");
+      ghbtn.classList.add("primary-btn");
+      ghbtn.innerHTML = `<a class="gh-link" href="${member.github}"><i  class="fab fa-github"></i></a>`;
+      teamMember.appendChild(ghbtn);
     });
-
-    
-})
-.catch(err => console.log(err))
+  })
+  .catch((err) => console.log(err));
--- js/contact-fetch.js
+++ js/contact-fetch.js
@@ -1,40 +1,31 @@
-window.onload=()=>{
-
-
-
-    fetch('http://localhost:1111/allcontacts', {
-    method: 'GET', // *GET, POST, PUT, DELETE, etc.
-    mode: 'cors', // no-cors, *cors, same-origin
+window.onload = () => {
+  fetch("http://localhost:1111/allcontacts", {
+    method: "GET", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, *cors, same-origin
     headers: {
-    'Content-Type': 'application/json'
-    },   // body data type must match "Content-Type" header
-}).then(res => res.json())
-  .then(data => {
+      "Content-Type": "application/json",
+    }, // body data type must match "Content-Type" header
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
 
-    console.log(data);
-
-    data.map((contact)=>{
+      data.map((contact) => {
         console.log(contact);
-        let grandParent = document.getElementById('responses-container');
-        var titleParent = document.createElement('div');
-        titleParent.classList.add('title-parent');
+        let grandParent = document.getElementById("responses-container");
+        var titleParent = document.createElement("div");
+        titleParent.classList.add("title-parent");
         grandParent.appendChild(titleParent);
 
-        
-    
-        var condiv = document.createElement('h2');
-        condiv.classList.add('contact-title')
-        condiv.innerHTML = contact.title
+        var condiv = document.createElement("h2");
+        condiv.classList.add("contact-title");
+        condiv.innerHTML = contact.title;
         titleParent.appendChild(condiv);
-        var tc = document.createElement('p');
-        tc.style.color="#fff";
+        var tc = document.createElement("p");
+        tc.style.color = "#fff";
         tc.innerHTML = contact.content;
-        condiv.appendChild(tc)
-
+        condiv.appendChild(tc);
+      });
     })
-
-
-    
-}).catch(err => console.log(err))
-
-}
+    .catch((err) => console.log(err));
+};
